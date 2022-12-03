6ec8c97ebacb377e9feebeb18c8bf20b78cfc9ec
mldamico <matias.lioneldamico@gmail.com>
2022-12-03 09:01:23 -0300

    Cart List

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
 import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
 
 const productsInCart = [
   initialData.products[0],
@@ -10,8 +13,31 @@ export const CartList = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <div key={product.slug}>
-          <h2>{product.slug}</h2>
+        <div key={product.slug} className="grid grid-cols-4 gap-2 my-2">
+          <div>
+            <Link href={`/slug`}>
+              <div>
+                <Image
+                  className="rounded-xl"
+                  height={200}
+                  width={200}
+                  alt={product.slug}
+                  src={`/products/${product.images[0]}`}
+                />
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col col-span-2">
+            <h2>{product.title}</h2>
+            <h3>
+              Size: <span className="font-bold">M</span>
+            </h3>
+            <ItemCounter />
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="text-lg">${product.price}</h3>
+            <button className="text-[#325AD0]">Remove</button>
+          </div>
         </div>
       ))}
     </>
