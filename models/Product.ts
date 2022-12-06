51419183d94f799b51fa983366fede6c6b068c69
mldamico <matias.lioneldamico@gmail.com>
2022-12-06 16:10:29 -0300

    Product schema on mongoose

---
+++ models/Product.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, model, Model } from "mongoose";
+import { IProduct } from "../interfaces";
+
+const productSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ["XS", "S", "M", "L", "XL", "XXL", "XXL"],
+          message: "{VALUE} is not an allowed size",
+        },
+      },
+    ],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: {
+      type: String,
+      enum: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{VALUE} is not an valid type",
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{VALUE} is not an valid gender",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product: Model<IProduct> =
+  mongoose.models.Product || model("Product", productSchema);
+
+export default Product;
