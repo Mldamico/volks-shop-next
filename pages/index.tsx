dfba90af1b6cba034584cee62b41f111687f768c
mldamico <matias.lioneldamico@gmail.com>
2022-12-07 16:48:05 -0300

    useSwr setup and products for db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import useSWR from "swr";
 import { ShopLayout } from "../components/layouts";
 import { ProductList } from "../components/products";
-import { initialData } from "../database/products";
+const fetcher = (...args: [key: string]) =>
+  fetch(...args).then((res) => res.json());
 
-export default function Home() {
+export default function HomePage() {
+  const { data, error } = useSWR("/api/products", fetcher);
+
+  if (error) return <div>Failed To load</div>;
+  if (!data) return <div>Loading...</div>;
   return (
     <ShopLayout
       title="Volks Shop - Home"
@@ -11,7 +17,7 @@ export default function Home() {
     >
       <h1 className="text-4xl">Volks Shop</h1>
       <h2 className="mb-1 text-2xl">All Products</h2>
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   );
 }
