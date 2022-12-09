7a82d6dd81dd1d029013635030531b0fba6f506b
mldamico <matias.lioneldamico@gmail.com>
2022-12-09 18:49:39 -0300

    Token generate

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../database";
 import { User } from "../../../models";
 import bcrypt from "bcryptjs";
+import { jwt } from "../../../utils";
 
 type Data =
   | {
@@ -51,10 +52,11 @@ async function loginUser(req: NextApiRequest, res: NextApiResponse<Data>) {
     return res.status(401).json({ message: "Invalid Credentials" });
   }
 
-  const { role, name } = user;
-  console.log(process.env.NODE_ENV);
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
   return res.status(200).json({
-    token: "",
+    token,
     user: {
       role,
       name,
---
+++ utils/jwt.ts
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+export const signToken = (_id: string, email: string) => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error("Error with JWT SEED");
+  }
+
+  return jwt.sign({ _id, email }, process.env.JWT_SECRET_SEED, {
+    expiresIn: "30d",
+  });
+};
