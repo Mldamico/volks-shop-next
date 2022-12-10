f048a5749192bdc61a469b0ff147699e4aef7f5f
mldamico <matias.lioneldamico@gmail.com>
2022-12-10 17:10:28 -0300

    Show error after trying to login with wrong credentials

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,12 +4,14 @@ import { useForm } from "react-hook-form";
 import { volksApi } from "../../api";
 import { AuthLayout } from "../../components/layouts";
 import { validations } from "../../utils";
-
+import { BiErrorCircle } from "react-icons/bi";
+import { useState } from "react";
 type FormData = {
   email: string;
   password: string;
 };
 const LoginPage = () => {
+  const [showError, setShowError] = useState(false);
   const {
     register,
     handleSubmit,
@@ -17,12 +19,17 @@ const LoginPage = () => {
   } = useForm<FormData>();
 
   const onLoginUser = async ({ email, password }: FormData) => {
+    setShowError(false);
     try {
       const { data } = await volksApi.post("/user/login", { email, password });
       const { token, user } = data;
       console.log({ token, user });
     } catch (error) {
+      setShowError(true);
       console.log(error);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
     }
   };
   return (
@@ -30,6 +37,12 @@ const LoginPage = () => {
       <form onSubmit={handleSubmit(onLoginUser)} noValidate>
         <div className="w-[350px] px-10 py-5">
           <h1 className="text-3xl md:text-4xl">Sign In</h1>
+          {showError && (
+            <div className="flex items-center px-4 py-2 my-2 space-x-4 text-white bg-red-500 rounded-3xl">
+              <BiErrorCircle size={18} />
+              <p className="text-md">Wrong email or password</p>
+            </div>
+          )}
           <div className="my-4 space-y-4">
             <div>
               <label htmlFor="email">Email</label>
