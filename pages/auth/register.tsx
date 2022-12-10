000861634d8458b791d758678d7e6dd7d5409e94
mldamico <matias.lioneldamico@gmail.com>
2022-12-10 17:53:13 -0300

    Register form hooks

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,48 +1,108 @@
 import Link from "next/link";
 import React from "react";
 import { AuthLayout } from "../../components/layouts";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { volksApi } from "../../api";
+import { BiErrorCircle } from "react-icons/bi";
+import { validations } from "../../utils";
 
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 const RegisterPage = () => {
+  const [showError, setShowError] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onRegisterForm = async ({ name, email, password }: FormData) => {
+    setShowError(false);
+    try {
+      const { data } = await volksApi.post("/user/register", {
+        name,
+        email,
+        password,
+      });
+      const { token, user } = data;
+      console.log({ token, user });
+    } catch (error) {
+      setShowError(true);
+      console.log(error);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+    }
+  };
   return (
     <AuthLayout title="Register">
-      <div className="w-[350px] px-10 py-5">
-        <h1 className="text-3xl md:text-4xl">Sign Up</h1>
-        <div className="my-4 space-y-4">
-          <div>
-            <label htmlFor="fullName">Full Name</label>
-            <input
-              type="text"
-              id="fullName"
-              placeholder="Full Name"
-              className="input"
-            />
+      <form onSubmit={handleSubmit(onRegisterForm)} noValidate>
+        <div className="w-[350px] px-10 py-5">
+          <h1 className="text-3xl md:text-4xl">Sign Up</h1>
+          {showError && (
+            <div className="flex items-center px-4 py-2 my-2 space-x-4 text-white bg-red-500 rounded-3xl">
+              <BiErrorCircle size={18} />
+              <p className="text-md">Email Already In Use</p>
+            </div>
+          )}
+          <div className="my-4 space-y-4">
+            <div>
+              <label htmlFor="fullName">Full Name</label>
+              <input
+                type="text"
+                id="fullName"
+                placeholder="Full Name"
+                className={!!errors.email ? "input-error" : "input"}
+                {...register("name", {
+                  required: "Field is required",
+                  minLength: {
+                    value: 2,
+                    message: "Should be have at least 2 characters",
+                  },
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                className={!!errors.email ? "input-error" : "input"}
+                {...register("email", {
+                  required: "Field is required",
+                  validate: validations.isEmail,
+                })}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className={!!errors.email ? "input-error" : "input"}
+                {...register("password", {
+                  required: "Field is required",
+                  minLength: {
+                    value: 6,
+                    message: "Should be have at least 6 characters",
+                  },
+                })}
+              />
+            </div>
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Email"
-              className="input"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Password"
-              className="w-full px-2 py-1 bg-gray-200 border-b-2 border-black outline-none"
-            />
+            <button className="button-secondary">Register</button>
+            <Link href={"/auth/login"} className="underline ">
+              Have an account?
+            </Link>
           </div>
         </div>
-        <div>
-          <button className="button-secondary">Register</button>
-          <Link href={"/auth/login"} className="underline ">
-            Have an account?
-          </Link>
-        </div>
-      </div>
+      </form>
     </AuthLayout>
   );
 };
