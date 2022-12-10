7c09124c27652750a178736a4049c8ce8258437c
mldamico <matias.lioneldamico@gmail.com>
2022-12-10 19:27:53 -0300

    Validate token in context

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { FC, useReducer } from "react";
+import { FC, useReducer, useEffect } from "react";
 import { AuthContext, authReducer } from "./";
 import { IUser } from "../../interfaces/user";
 import { volksApi } from "../../api";
@@ -20,6 +20,21 @@ type Props = {
 
 export const AuthProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = async () => {
+    try {
+      const { data } = await volksApi.get("/user/validate-token");
+      const { token, user } = data;
+      Cookies.set("token", token);
+      dispatch({ type: "Auth - Login", payload: user });
+    } catch (error) {
+      Cookies.remove("token");
+    }
+  };
 
   const loginUser = async (email: string, password: string) => {
     try {
