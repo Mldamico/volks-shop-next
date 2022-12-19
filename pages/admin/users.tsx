19c5d9edcb2d07ce64de7b55dba7888dfc4d65ce
mldamico <matias.lioneldamico@gmail.com>
2022-12-19 08:29:52 -0300

    Change role from user

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -7,15 +7,45 @@ import useSWR from "swr";
 import { IUser } from "../../interfaces";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { volksApi } from "../../api";
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>("/api/admin/users");
   if (!data && !error) return <></>;
 
+  const onRoleUpdated = async (userId: string, newRole: string) => {
+    try {
+      await volksApi.put("/admin/users", { userId, role: newRole });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const columnDefs = [
     { headerName: "Email", field: "email", width: 250 },
     { headerName: "Full Name", field: "name", width: 250 },
-    { headerName: "Role", field: "role", width: 200 },
+    {
+      headerName: "Role",
+      field: "role",
+      width: 200,
+      cellRenderer: (props: any) => {
+        console.log(props);
+        return (
+          <select
+            value={props.data.role}
+            className="w-[100px]"
+            onChange={({ target }) =>
+              onRoleUpdated(props.data.id, target.value)
+            }
+          >
+            <option value="admin">Admin</option>
+            <option value="client">Client</option>
+            <option value="super-user">Super-User</option>
+            <option value="SEO">SEO</option>
+          </select>
+        );
+      },
+    },
   ];
 
   const rowData = data!.map((user) => ({
